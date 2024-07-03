78509e8c46d9801549a971e5aa73e62a785bd620
Ray <alvi.rayhan@gmail.com>
2024-07-03 19:42:53 -0400

    "Added RouterModule to AppNavbarModule and updated NavMenuComponent to use routerLink instead of href"

--- src/app/app-navbar/app-navbar.module.ts
+++ src/app/app-navbar/app-navbar.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LogoComponent } from './components/logo/logo.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { MainNavbarComponent } from './components/main-navbar/main-navbar.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -16,7 +17,8 @@ import { MainNavbarComponent } from './components/main-navbar/main-navbar.compon
     MainNavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class AppNavbarModule { }
